b0e4e953cc5c9944f8256b7a4fbd96dfba7e712a
miracle <miracle@tencent.com>
2020-08-13 22:13:10 +0800

    feat(): update webpack directory

--- webpack/backup/src/static/scripts/image.js
+++
@@ -1,8 +0,0 @@
-import demo from '../images/demo.png';
-
-const image = new Image();
-// image.src = require('../images/demo.png');
-image.src = demo;
-image.width = 150;
-image.height = 100;
-document.body.appendChild(image);
