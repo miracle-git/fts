797ccc5864c1f76741654f706cca16bf727bbc68
miracle <miracle@tencent.com>
2020-08-16 19:52:36 +0800

    feat(): update hashtable

--- js/data-structure/06-哈希表(HashTable)/src/hashtable.js
+++ js/data-structure/06-哈希表(HashTable)/src/hashtable.js
@@ -6,8 +6,9 @@ export default class HashTable {
     this.length = 0
     this.capacity = capacity
   }
-
+  // 静态私有函数
   static [ht] = {
+    // 判断当前数字是否为质数
     isPrime(num) {
       const res = parseInt(Math.sqrt(num))
       for (let i = 2; i <= res; i++) {
@@ -15,6 +16,7 @@ export default class HashTable {
       }
       return true
     },
+    // 获取离当前数字最近的质数
     getPrime(num) {
       while(!HashTable[ht].isPrime(num)) {
         num++
@@ -22,10 +24,7 @@ export default class HashTable {
       return num
     }
   }
-
+  // 向哈希表中填充数据
   put(key, val) {
-    const res = HashTable[ht].isPrime(35)
-    console.log(res)
-    console.log(HashTable[ht].getPrime(32))
   }
 }
