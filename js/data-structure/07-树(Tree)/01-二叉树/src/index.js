a449e40e3436e184a90f36d031d454d18bbfb917
miracle <miracle@tencent.com>
2020-08-21 22:53:45 +0800

    feat(): update tree

--- js/data-structure/07-树(Tree)/01-二叉树/src/index.js
+++ js/data-structure/07-树(Tree)/01-二叉树/src/index.js
@@ -3,4 +3,12 @@ import BinarySearchTree from "./tree";
 
 const bst = new BinarySearchTree()
 
-bst.insert(10)
+const keys = [11, 7, 15, 5, 3, 6, 9, 8, 10, 13, 12, 14, 20, 18, 25]
+keys.forEach(item => bst.insert(item))
+console.log(`先序遍历：${bst.preOrder()}`)
+console.log(`中序遍历：${bst.midOrder()}`)
+console.log(`后序遍历：${bst.postOrder()}`)
+console.log(`最小值：${bst.min()}`)
+console.log(`最大值：${bst.max()}`)
+console.log(`查找18：${bst.search(18)}`)
+console.log(`查找24：${bst.search(24)}`)
--- js/data-structure/07-树(Tree)/01-二叉树/src/tree.js
+++ js/data-structure/07-树(Tree)/01-二叉树/src/tree.js
@@ -1,19 +1,20 @@
-// 二叉搜索树(BST)
-class Node {
+// 私有标记Symbol
+const _bst_s = Symbol('bst static methods')
+const _bst_i = Symbol('bst instance methods')
+// 二叉搜索树节点
+class TreeNode {
   constructor(key) {
     this.key = key
     this.left = null
     this.right = null
   }
 }
-
-const bsts = Symbol('bst static methods')
-const bsti = Symbol('bst instance methods')
-
+// 二叉搜索树(BST)
 export default class BinarySearchTree {
   constructor() {
     this.root = null // 根节点
-    this[bsti] = {
+    // 私有实例成员
+    this[_bst_i] = {
       insertNode: (node, newNode) => {
         // 判断新节点应该插入node的左子树还是右子树
         if (newNode.key < node.key) {
@@ -21,31 +22,90 @@ export default class BinarySearchTree {
           if (!node.left) {
             node.left = newNode
           } else {
-            this[bsti].insertNode(node.left, newNode)
+            this[_bst_i].insertNode(node.left, newNode)
           }
         } else {
           // 判断右子树是否存在
           if (!node.right) {
             node.right = newNode
           } else {
-            this[bsti].insertNode(node.right, newNode)
-          }ß
+            this[_bst_i].insertNode(node.right, newNode)
+          }
+        }
+      },
+      traverseNode: (node, order, handler) => {
+        if (node) {
+          order === 'pre' && handler(node.key)
+          this[_bst_i].traverseNode(node.left, order, handler)
+          order === 'mid' && handler(node.key)
+          this[_bst_i].traverseNode(node.right, order, handler)
+          order === 'post' && handler(node.key)
+        }
+      },
+      traverseOrder: (order) => {
+        let res = ''
+        this[_bst_i].traverseNode(this.root, order, key => res += res ? '->' + key : key)
+        return res
+      },
+      searchNode: (node, key) => {
+        if (!node) return false
+        if (key < node.key) {
+          // 如果传入的key比当前节点的key小，从左子树找
+          return this[_bst_i].searchNode(node.left, key)
+        } else if (key > node.key) {
+          // 如果传入的key比当前节点的key大，从右子树找
+          return this[_bst_i].searchNode(node.right, key)
+        } else {
+          // 在此已经找到key了
+          return true
         }
       }
     }
   }
   // 私有静态成员
-  static [bsts] = {
+  static [_bst_s] = {
   }
   // 插入节点
   insert(key) {
     // 初始化新节点
-    const newNode = new Node(key)
+    const newNode = new TreeNode(key)
     // 判断根节点是否存在
     if (!this.root) {
       this.root = newNode
     } else {
-      this[bsti].insertNode(this.root, newNode)
+      this[_bst_i].insertNode(this.root, newNode)
     }
+  }
+  // 先序遍历
+  preOrder() {
+    return this[_bst_i].traverseOrder('pre')
+  }
+  // 中序遍历
+  midOrder() {
+    return this[_bst_i].traverseOrder('mid')
+  }
+  // 后序遍历
+  postOrder() {
+    return this[_bst_i].traverseOrder('post')
+  }
+  // 最小值
+  min() {
+    let node = this.root || {}
+    while (node.left) {
+      node = node.left
+    }
+    return node.key || ''
+  }
+  // 最大值
+  max() {
+    let node = this.root || {}
+    while (node.right) {
+      node = node.right
+    }
+    return node.key || ''
+  }
+  // 搜索特定的key
+  search(key) {
+    return this[_bst_i].searchNode(this.root, key)
   }
 }
