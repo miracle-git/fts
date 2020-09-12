df4b327cbcfbe1d393a52b616364861d116ce92c
miracle <miracle@tencent.com>
2020-09-12 10:59:38 +0800

    feat(): update sort

--- js/data-algorithm/01-排序算法/07-堆排序(HeapSort)/src/sorter.js
+++ js/data-algorithm/01-排序算法/07-堆排序(HeapSort)/src/sorter.js
@@ -18,12 +18,36 @@ export default class Sorter {
         } else {
           return direction.toLowerCase() === 'asc' ? a > b : a < b
         }
+      },
+      heapify: (i, len, direction) => {
+        const left = 2 * i + 1, right = 2 * i + 2
+        let temp = i
+        if (left < len && this[_st_i].compare(left, temp, { direction })) {
+          temp = left
+        }
+        if (right < len && this[_st_i].compare(right, temp, { direction })) {
+          temp = right
+        }
+        if (temp !== i) {
+          this[_st_i].swap(temp, i)
+          this[_st_i].heapify(temp, len, direction)
+        }
       }
     }
   }
   // 堆排序
   heapSort(direction = 'asc') {
-    const len = this.items.length
+    let len = this.items.length
+    let mid = Math.floor(len / 2)
+    // 建立大顶堆
+    for (let i = mid; i >= 0; i--) {
+      this[_st_i].heapify(i, len, direction)
+    }
+    for (let i = len - 1; i > 0; i--) {
+      this[_st_i].swap(0, i)
+      len--
+      this[_st_i].heapify(0, len, direction)
+    }
     return this.items
   }
 }
