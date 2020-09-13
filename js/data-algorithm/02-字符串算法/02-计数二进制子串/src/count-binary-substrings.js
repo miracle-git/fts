763ebb23ffdf4d4149a3584dd882fad71a95aec8
miracle <miracle@tencent.com>
2020-09-13 21:02:57 +0800

    feat(): update string

--- js/data-algorithm/02-字符串算法/02-计数二进制子串/src/count-binary-substrings.js
+++ js/data-algorithm/02-字符串算法/02-计数二进制子串/src/count-binary-substrings.js
@@ -22,22 +22,28 @@
  */
 const match = (str) => {
   const res = []
+  // 2.1 如何检查匹配：将当前的字符串拆分转化为数组，并给0和1分别生成计数器(默认为0)
   const arr = str.split('')
   const counter = { '0': 0, '1': 0 }
   for (let i = 0; i < arr.length;i++) {
     const item = arr[i]
+    // 2.2 循环数组中的每一项，首先加入到匹配结果数组再将对应的计数器加1
     res.push(item)
     counter[item]++
-    if (counter['0'] === counter['1']) break
+    // 2.3 如果发现0和1和计数器已相等，提前结束循环并返回结果
+    if (counter['0'] === counter['1']) return res.join('')
   }
-  if (counter['0'] !== counter['1']) return ''
-  return res.join('')
+  // 2.4 如果到循环结束，则直接返回空字符串
+  return ''
 }
 
 export const countBinarySubstrings = (str) => {
   const res = []
+  // 1. 每次将字符串像后移动1位作为输入
   for (let i = 0; i < str.length - 1; i++) {
+    // 2. 检查当前子字符串是否存在满足条件的匹配
     const item = match(str.slice(i))
+    // 3. 如果存在匹配，则将匹配加入到结果数组
     if (item) {
       res.push(item)
     }
