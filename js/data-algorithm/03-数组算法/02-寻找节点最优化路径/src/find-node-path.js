5a50df5728bff0701a25a7457271750630abd878
Miracle He <hmiinyu@sina.com>
2021-07-01 23:28:52 +0800

    更新算法源码文件

---
+++ js/data-algorithm/03-数组算法/02-寻找节点最优化路径/src/find-node-path.js
@@ -0,0 +1,81 @@
+/**
+ * 有这么一个数据结构:
+ * const data = [
+ * {
+ *    "id": "1",
+ *    "sub": [{
+ *        "id": "2",
+ *        "sub": [
+ *          { "id": "3", "sub": null },
+ *          {
+ *             "id": "4", "sub": [
+ *                { "id": "6", "sub": null }
+ *             ]
+ *          },
+ *          { "id": "5", "sub": null }
+ *        ]
+ *      }
+ *    ]
+ *  },
+ *  {
+ *    "id": "7",
+ *    "sub": [{
+ *        "id": "8", "sub": [
+ *           { "id": "9", "sub": null }
+ *        ]
+ *      }
+ *    ]
+ *  },
+ *  { "id": "10", "sub": null }
+ *]
+ 现在给定一个id，要求实现一个函数
+
+ findNodePath(data, id) {}
+ 返回给定id在 data 里的路径
+
+ 示例:
+ id = "1" => ["1"]
+ id = "9" => ["7", "8", "9"]
+ id = "100"=> []
+ PS: id 全局唯一，无序
+ */
+export const findNodePath1 = (data, id) => {
+  let arr = [], exist = false;
+
+  (function find($data, $id) {
+    for (let item of $data) {
+      if (exist) return arr
+      exist = item.id === $id
+      if (exist) return arr.push(item.id)
+
+      if (Array.isArray(item.sub)) {
+        arr.push(item.id)
+        find(item.sub, id)
+        !exist && arr.pop()
+      }
+    }
+  })(data, id)
+
+  return arr
+}
+
+export const findNodePath2 = (data, id) => {
+  let arr = [];
+
+  (function find($data, $id, $path) {
+    if (!Array.isArray($data)) return []
+    if (!Array.isArray($path)) $path = []
+
+    for (let item of $data) {
+      if (item.id === id) {
+        $path.push(item.id)
+        arr = $path
+        return arr
+      }
+
+      find(item.sub, id, [...$path, item.id])
+    }
+  })(data, id)
+
+  return arr
+}
---
+++ js/data-algorithm/03-数组算法/02-寻找节点最优化路径/src/index.js
@@ -0,0 +1,26 @@
+import { findNodePath1, findNodePath2 } from './find-node-path'
+
+const data = [
+  {
+    "id": "1",
+    "sub": [
+      {
+        "id": "2",
+        "sub": [
+          {
+            "id": "3",
+            "sub": null
+          }
+        ]
+      }
+    ]
+  }
+]
+
+console.log(findNodePath1(data, '1'))
+console.log(findNodePath1(data, '2'))
+console.log(findNodePath1(data, '3'))
+
+console.log(findNodePath2(data, '1'))
+console.log(findNodePath2(data, '2'))
+console.log(findNodePath2(data, '3'))
