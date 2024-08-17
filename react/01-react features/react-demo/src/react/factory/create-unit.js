f88a0b4688384bff5e67c5df42186d27e6fc4417
miracle <hmiinyu@sina.com>
2024-08-18 00:59:22 +0800

    feat: implement setState method

--- react/01-react features/react-demo/src/react/factory/create-unit.js
+++ react/01-react features/react-demo/src/react/factory/create-unit.js
@@ -1,17 +1,16 @@
-import ReactElement from '../react/element'
 import ReactTextUnit from '../unit/text-unit'
 import ReactNativeUnit from '../unit/native-unit'
 import ReactCompositeUnit from '../unit/composite-unit'
-import { includeTypes } from '../utils/type'
+import { includeTypes, isReactElement } from '../utils/type'
 
 export default function createUnit(element) {
   if (includeTypes(element, 'string', 'number')) {
     return new ReactTextUnit(element)
   }
-  if (element instanceof ReactElement && typeof element.type === 'string') {
+  if (isReactElement(element) && typeof element.type === 'string') {
     return new ReactNativeUnit(element)
   }
-  if (element instanceof ReactElement && typeof element.type === 'function') {
+  if (isReactElement(element) && typeof element.type === 'function') {
     return new ReactCompositeUnit(element)
   }
 }
--- react/01-react features/react-demo/src/react/utils/type.js
+++ react/01-react features/react-demo/src/react/utils/type.js
@@ -1,8 +1,15 @@
+import ReactElement from '../react/element'
+
 function includeTypes(element, ...types) {
   const elements = Array.isArray(element) ? element : [element]
   return elements.every(item => types.includes(typeof item))
 }
 
+function isReactElement(...elements) {
+  return elements.every(item => item instanceof ReactElement)
+}
+
 export {
-  includeTypes
+  includeTypes,
+  isReactElement
 }
