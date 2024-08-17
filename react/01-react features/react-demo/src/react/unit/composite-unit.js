635d2e94257cc5559363f76129844eb1df678b67
miracle <hmiinyu@sina.com>
2024-08-17 11:51:31 +0800

    feat: refactor react demo

--- react/01-react features/react-demo/src/react/unit/composite-unit.js
+++ react/01-react features/react-demo/src/react/unit/composite-unit.js
@@ -5,8 +5,8 @@ import createUnit from '../factory/create-unit'
 export default class ReactCompositeUnit extends ReactUnit {
   getMarkup(reactid) {
     super.getMarkup(reactid)
-    const { type: Component, props } = this.element
-    this.component = new Component(props)
+    const { type: ReactComponent, props } = this.element
+    this.component = new ReactComponent(props)
     this.component.currentUnit = this
     this.component.componentWillMount && this.component.componentWillMount()
     const element = this.component.render()
